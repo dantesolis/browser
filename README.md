3b4c59d384318d6c02a697416860c1875f659156
dantesolis <dantevs@icloud.com>
2016-12-19 13:45:07 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+## CSS and JS Browser Window
+
+### README
+
+A simple browser window made with CSS &amp; JS. A Simple `js` and `css` project. See the live version here [cssbrowser](http://cssbrowser.site)
+
+![browserWindow screenshot](src/img/browserWindow.png)
+
+### Install
+ Clone on your local machine and run `npm i` or just view the [demo](https://codepen.io/intercoder/pen/84fe8fb5d06335fc1ed0d084474e9aa0) on codepen.
+
+### TODO
+- [x] Clean codepen demo - add link
+- [ ] Publish to github pages
+- [x] Add link to github repo
+- [x] Add license
+- [x] Add domain
+- [x] Add gulp task
+- [ ] Add Google Analytics
+- [ ] Write Case Study
+- [ ] Publish `TypeIt` function as a `npm package`
+- [x] Add pictures to readme
+
+
+Made with ❤  2016 Belgium - D/S
---
+++ src/js/type.js
@@ -0,0 +1,48 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+  /*
+  * @const innerWindowEl {object}
+  * @const contentTypedEl {object}
+  * @const windowText {string}
+  * @const pText {string}
+  * @let nTimerId {number} [undefined]
+  */
+  const innerWindowEl  = document.querySelector('.window--inner');
+  const contentTypedEl = document.querySelector('.content--typed');
+
+  const windowText = 'Hello from Js and Css';
+  const pText      = 'Things tackled here. BEM terminology, ES6, CSS animations, Scss/Js loops and Js timers';
+
+  let nTimerId;
+
+
+  /*
+  * @function typeIt - takes text and splits it and writes each val in the arry in a DOM obj.
+  * @global
+  * @param {string} - string
+  * @param {object} - DOM object
+  */
+  function typeIt(str, el) {
+    el.innerHTML = '';
+    let arry     = str.split('');
+
+    for (var i = 0; i < arry.length; i++) {
+      (function(i) {
+      
+        nTimerId = setTimeout( () => {
+          el.innerHTML += arry[i];
+
+          if ( (i + 1) == arry.length ) {
+            setTimeout( () => {
+              typeIt(str, el);
+            }, 5000);
+          }
+
+        }, 300 * i);
+      }(i));
+    } /* end of for loop */
+  }
+
+  typeIt(windowText, innerWindowEl);
+  typeIt(pText, contentTypedEl);
+})
